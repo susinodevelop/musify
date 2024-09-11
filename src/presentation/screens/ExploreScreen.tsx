dd3798a38df5b91f811c1a2b7841d5dc01c19ebf
Suso <susinodevelop@gmail.com>
2024-09-11 10:04:40 +0200

    Añade activity indicator a la carga de la biblioteca

--- src/presentation/screens/ExploreScreen.tsx
+++ src/presentation/screens/ExploreScreen.tsx
@@ -1,18 +1,72 @@
-import React, { useContext } from "react";
-import { Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Text, StyleSheet, FlatList } from "react-native";
 import ScreenWithPlayer from "../components/ScreenWithPlayer";
 import { ThemeContext } from "../context/ThemeContext";
+import { useRepositories } from "../context/RepositoryContext";
+import PlaylistEntity from "@/domain/entities/PlaylistEntity";
+import PlaylistCard from "../components/PlaylistCard";
+import { ScrollView } from "react-native-gesture-handler";
 
 const ExploreScreen = () => {
+  const { playlistRepository } = useRepositories();
   const { themeColors } = useContext(ThemeContext);
+
+  const [newReleases, setNewReleases] = useState<PlaylistEntity[]>([]);
+  const [recommended, setRecommended] = useState<PlaylistEntity[]>([]);
+
+  useEffect(() => {
+    playlistRepository.getRandom("newReleases", 5).then(setNewReleases);
+    playlistRepository.getRandom("recommended", 5).then(setRecommended);
+  }, []);
 
   return (
     <ScreenWithPlayer>
-      <View>
-        <Text style={{ color: themeColors.text }}>Explore Screen</Text>
-      </View>
+      <ScrollView style={[styles.container]}>
+        <Text style={[styles.sectionTitle, { color: themeColors.text }]}>
+          Nuevos Lanzamientos
+        </Text>
+        <FlatList
+          data={newReleases}
+          renderItem={({ item }) => <PlaylistCard playlist={item} />}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.listContainer}
+        />
+
+        <Text style={[styles.sectionTitle, { color: themeColors.text }]}>
+          Recomendados para Ti
+        </Text>
+        <FlatList
+          data={recommended}
+          renderItem={({ item }) => <PlaylistCard playlist={item} />}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.listContainer}
+        />
+
+        <Text style={[styles.sectionTitle, { color: themeColors.text }]}>
+          Tus Playlists
+        </Text>
+        {/* Aquí puedes añadir otra FlatList para las playlists del usuario */}
+      </ScrollView>
     </ScreenWithPlayer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  listContainer: {
+    paddingBottom: 20,
+  },
+});
 
 export default ExploreScreen;
--- src/presentation/screens/LibraryScreen.tsx
+++ src/presentation/screens/LibraryScreen.tsx
@@ -3,7 +3,7 @@ import PlaylistEntity from "@/domain/entities/PlaylistEntity";
 import TrackEntity from "@/domain/entities/TrackEntity";
 import React, { useContext, useEffect, useState } from "react";
 import { View, ScrollView, StyleSheet, Image, Pressable } from "react-native";
-import { Button, Text, List } from "react-native-paper";
+import { Button, Text, List, ActivityIndicator } from "react-native-paper";
 import { useRepositories } from "../context/RepositoryContext";
 import ScreenWithPlayer from "../components/ScreenWithPlayer";
 import { ThemeContext } from "../context/ThemeContext";
@@ -20,134 +20,148 @@ const LibraryScreen: React.FC = () => {
   const [playlists, setPlaylists] = useState<PlaylistEntity[]>([]);
   const [tracks, setTracks] = useState<TrackEntity[]>([]);
   const [artists, setArtists] = useState<ArtistEntity[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    playlistRepository.getRandom("a", 5).then(setPlaylists);
-    trackRepository.getRandom("a", 5).then(setTracks);
-    artistRepository.getRandom("a", 5).then(setArtists);
+    setIsLoading(true);
+    const promisePlaylists = playlistRepository
+      .getRandom("a", 5)
+      .then(setPlaylists);
+    const promiseTracks = trackRepository.getRandom("a", 5).then(setTracks);
+    const promiseArtists = artistRepository.getRandom("a", 5).then(setArtists);
+
+    Promise.all([promisePlaylists, promiseTracks, promiseArtists]).then(() =>
+      setIsLoading(false)
+    );
   }, []);
 
   return (
     <ScreenWithPlayer>
-      <View
-        style={{
-          backgroundColor: themeColors.screenBackground,
-          ...styles.container,
-        }}
-      >
-        <ScrollView style={styles.mainContent}>
-          <View style={styles.section}>
-            <Text
-              variant="titleLarge"
-              style={{ color: themeColors.title, ...styles.sectionTitle }}
-            >
-              Playlists
-            </Text>
-            <Button
-              mode="contained"
-              onPress={() => {}}
-              style={styles.addButton}
-            >
-              Añadir Nueva Playlist
-            </Button>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {playlists.map((playlist) => (
-                <PlaylistCard key={playlist.id} playlist={playlist} />
-              ))}
-            </ScrollView>
-          </View>
+      {isLoading ? (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator
+            size="large"
+            color={themeColors.activityIndicator}
+          />
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <ScrollView style={styles.mainContent}>
+            <View style={styles.section}>
+              <Text
+                variant="titleLarge"
+                style={{ color: themeColors.title, ...styles.sectionTitle }}
+              >
+                Playlists
+              </Text>
+              <Button
+                mode="contained"
+                onPress={() => {}}
+                style={styles.addButton}
+              >
+                Añadir Nueva Playlist
+              </Button>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {playlists.map((playlist) => (
+                  <PlaylistCard key={playlist.id} playlist={playlist} />
+                ))}
+              </ScrollView>
+            </View>
 
-          <View style={styles.section}>
-            <Text
-              variant="titleLarge"
-              style={{ color: themeColors.title, ...styles.sectionTitle }}
-            >
-              Canciones Favoritas
-            </Text>
+            <View style={styles.section}>
+              <Text
+                variant="titleLarge"
+                style={{ color: themeColors.title, ...styles.sectionTitle }}
+              >
+                Canciones Favoritas
+              </Text>
 
-            <List.Section>
-              {tracks.map((track) => (
-                <Pressable key={track.id} onPress={() => load(track)}>
-                  <List.Item
-                    title={track.title}
-                    description={`${track.artist.name} - ${track.genre}`}
-                    titleStyle={{ color: themeColors.title }}
-                    descriptionStyle={{ color: themeColors.text }}
-                    left={() => (
-                      <Image
-                        source={{ uri: track.cover }}
-                        width={50}
-                        height={50}
-                        borderRadius={5}
-                      />
-                    )}
-                  />
-                </Pressable>
-              ))}
-            </List.Section>
-          </View>
+              <List.Section>
+                {tracks.map((track) => (
+                  <Pressable key={track.id} onPress={() => load(track)}>
+                    <List.Item
+                      title={track.title}
+                      description={`${track.artist.name} - ${track.genre}`}
+                      titleStyle={{ color: themeColors.title }}
+                      descriptionStyle={{ color: themeColors.text }}
+                      left={() => (
+                        <Image
+                          source={{ uri: track.cover }}
+                          width={50}
+                          height={50}
+                          borderRadius={5}
+                        />
+                      )}
+                    />
+                  </Pressable>
+                ))}
+              </List.Section>
+            </View>
 
-          <View style={styles.section}>
-            <Text
-              variant="titleLarge"
-              style={{ color: themeColors.title, ...styles.sectionTitle }}
-            >
-              Artistas
-            </Text>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {artists.map((artist) => (
-                <AritstCard key={artist.id} artist={artist} />
-              ))}
-            </ScrollView>
-          </View>
+            <View style={styles.section}>
+              <Text
+                variant="titleLarge"
+                style={{ color: themeColors.title, ...styles.sectionTitle }}
+              >
+                Artistas
+              </Text>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {artists.map((artist) => (
+                  <AritstCard key={artist.id} artist={artist} />
+                ))}
+              </ScrollView>
+            </View>
 
-          <View style={styles.section}>
-            <Text
-              variant="titleLarge"
-              style={{ color: themeColors.title, ...styles.sectionTitle }}
-            >
-              Descargas
-            </Text>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {tracks.map((track) => (
-                <TrackCard key={track.id} track={track} />
-              ))}
-            </ScrollView>
-          </View>
+            <View style={styles.section}>
+              <Text
+                variant="titleLarge"
+                style={{ color: themeColors.title, ...styles.sectionTitle }}
+              >
+                Descargas
+              </Text>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                {tracks.map((track) => (
+                  <TrackCard key={track.id} track={track} />
+                ))}
+              </ScrollView>
+            </View>
 
-          <View style={styles.section}>
-            <Text
-              variant="titleLarge"
-              style={{ color: themeColors.title, ...styles.sectionTitle }}
-            >
-              Historial
-            </Text>
-            <List.Section>
-              {tracks.map((track) => (
-                <Pressable key={track.id} onPress={() => load(track)}>
-                  <List.Item
-                    title={track.title}
-                    description={track.description}
-                    titleStyle={{ color: themeColors.title }}
-                    descriptionStyle={{ color: themeColors.text }}
-                    left={() => (
-                      <List.Icon icon="history" color={themeColors.text} />
-                    )}
-                    right={() => (
-                      <Image
-                        source={{ uri: track.cover }}
-                        width={50}
-                        height={50}
-                        style={{ borderRadius: 5, marginLeft: 10 }}
-                      />
-                    )}
-                  />
-                </Pressable>
-              ))}
-            </List.Section>
-          </View>
-        </ScrollView>
-      </View>
+            <View style={styles.section}>
+              <Text
+                variant="titleLarge"
+                style={{ color: themeColors.title, ...styles.sectionTitle }}
+              >
+                Historial
+              </Text>
+              <List.Section>
+                {tracks.map((track) => (
+                  <Pressable key={track.id} onPress={() => load(track)}>
+                    <List.Item
+                      title={track.title}
+                      description={track.description}
+                      titleStyle={{ color: themeColors.title }}
+                      descriptionStyle={{ color: themeColors.text }}
+                      left={() => (
+                        <List.Icon icon="history" color={themeColors.text} />
+                      )}
+                      right={() => (
+                        <Image
+                          source={{ uri: track.cover }}
+                          width={50}
+                          height={50}
+                          style={{ borderRadius: 5, marginLeft: 10 }}
+                        />
+                      )}
+                    />
+                  </Pressable>
+                ))}
+              </List.Section>
+            </View>
+          </ScrollView>
+        </View>
+      )}
     </ScreenWithPlayer>
   );
 };
